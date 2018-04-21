4d9c52d1fd577fabb0af68931e4388ea047da807
Zeying Li <admin@zeyingli.com>
2018-04-21 14:47:44 -0400

    added global setting config

---
+++ Snake/src/Settings.java
@@ -0,0 +1,8 @@
+/**
+ *
+ * @author GR10: Zeying, Tiancheng, Cheng, Jingyi
+ */
+public class Settings {
+    public static final long DEFAULT_MOVE_INTERVAL = 200;
+    public static int DEFAULT_NODE_SIZE = 10;
+}
