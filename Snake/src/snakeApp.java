3e32333360e4573ad5fc44ae21735e3fe43bcfe9
bunny9999 <guanjingyi1999@hotmail.com>
2018-04-21 15:42:56 -0400

    Create window

--- Snake/src/snakeApp.java
+++ Snake/src/snakeApp.java
@@ -2,11 +2,21 @@
  *
  * @author GR10: Zeying, Tiancheng, Cheng, Jingyi
  */
+import javax.swing.*;
 public class snakeApp {
 
+    public void init(){
+        JFrame window = new JFrame("Snake Game");
+        window.pack();
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+        
+    }
     public static void main(String[] args) 
     {
-        
+        snakeApp snakeApp = new snakeApp();
+        snakeApp.init();
     }
     
 }
