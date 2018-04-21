5ebfd8df29b4c7a75a4fa37106ca3e4f3175a45c
Zeying Li <admin@zeyingli.com>
2018-04-21 16:40:07 -0400

    Integrated JPanel inside JFrame

--- Snake/src/snakeApp.java
+++ Snake/src/snakeApp.java
@@ -3,6 +3,7 @@
  * @author GR10: Zeying, Tiancheng, Cheng, Jingyi
  */
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.*;
 public class snakeApp {
@@ -10,10 +11,21 @@ public class snakeApp {
     public void init(){
         JFrame window = new JFrame("Snake Game");
         Grid myGrid = new Grid(50*Settings.DEFAULT_NODE_SIZE, 50*Settings.DEFAULT_NODE_SIZE);
+        
+        SnakeView snakeView = new SnakeView(myGrid);
+        snakeView.init();
+        
         window.setPreferredSize(new Dimension(500,500));
+        
+        window.add(snakeView.getCanvas(), BorderLayout.CENTER);
+        
+        snakeView.draw();
+        
         window.setResizable(false);
-        window.pack();
+            
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        window.pack();
         window.setVisible(true);
         
     }
