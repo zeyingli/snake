743a5e2b7d73ab897373778464b7bb13f07fec3e
bunny9999 <guanjingyi1999@hotmail.com>
2018-04-21 15:54:56 -0400

    Finish JFrame and some settings

--- Snake/src/snakeApp.java
+++ Snake/src/snakeApp.java
@@ -2,13 +2,17 @@
  *
  * @author GR10: Zeying, Tiancheng, Cheng, Jingyi
  */
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import javax.swing.*;
 public class snakeApp {
 
     public void init(){
         JFrame window = new JFrame("Snake Game");
-        window.pack();
+        Grid myGrid = new Grid(50*Settings.DEFAULT_NODE_SIZE, 50*Settings.DEFAULT_NODE_SIZE);
+        window.setPreferredSize(new Dimension(500,500));
         window.setResizable(false);
+        window.pack();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
         
